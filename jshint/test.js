4a9254868f7eef16f1c1d5cc8ecc18a370bd9593
Zack Grossbart <zack@grossbart.com>
2014-10-27 15:03:48 -0400

    Added the rest of the second case and the third case to the rough draft.

--- jshint/test.js
+++ jshint/test.js
@@ -1,4 +1,24 @@
 function test() {
-    myVar = 'Hello, World';
-    console.log(myVar);
+    var myVar = 'Hello, World';
+    console.log(myvar);
+}
+
+var nOne = 1;
+var sOne = '1';
+
+if (nOne == sOne) {
+    alert('The two values were equal');
+}
+
+if (nOne === sOne) {
+    alert('The two values were equal');
+}
+
+function incrementCount(counter) {
+    if (counter.count) {
+        counter.count++;
+    } else {
+        var counter = 1;
+        counter.count = counter;
+    }    
 }
